51f513c633c52d38585993a16b7e76c9f6200144
Raj Jain <ritu65raj3@gmail.com>
2020-05-28 16:53:28 +0530

    May Week 2 commit

---
+++ May LeetCoding Challenge/Week 2 (May 8th – May 14th)/12 Single Element in a Sorted Array.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        // binary search
+        int n=nums.length, lo=0, hi=n/2;
+        while (lo < hi) {
+            int m = (lo + hi) / 2;
+            if (nums[2*m]!=nums[2*m+1]) hi = m;
+            else lo = m+1;
+        }
+        return nums[2*lo];
+    }
+}
